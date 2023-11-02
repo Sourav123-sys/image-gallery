8523fc4d2f6552e85467578af950dc76ab6a374e
Sourav123-sys <khansourav58@gmail.com>
2023-11-03 00:33:16 +0600

    toast add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,40 +7,42 @@ import React from "react";
 const GalleryHeader = ({
     selectPictures,
     setSelectPictures,
-  handleDeleteFile,
+    handleDeleteFile,
 }) => {
-  return (
-    <nav className="py-4 px-6">
-      <article className="flex flex-row justify-between items-center">
-        <h1 className="text-2xl font-bold">
-          {selectPictures.length === 0 ? (
-            "Gallery"
-          ) : (
-            <label
-              htmlFor="select"
-              className="flex flex-row justify-between items-center gap-x-4"
-            >
-              <input
-                type="checkbox"
-                name="select"
-                id="select"
-                checked={selectPictures.length > 0}
-                className="h-5 w-5 accent-blue-500 cursor-pointer"
-                onChange={() => setSelectPictures([])}
-              />
-              {selectPictures.length} Files Selected
-            </label>
-          )}
-        </h1>
-        <button
-          className="text-red-500 font-medium hover:underline"
-          onClick={handleDeleteFile}
-        >
-          Delete files
-        </button>
-      </article>
-    </nav>
-  );
+    return (
+        <nav className="py-4 px-6">
+            <article className="flex flex-row justify-between items-center">
+                <h1 className="text-2xl font-bold ">
+                    {selectPictures.length === 0 ? (
+                        <span className=" animate-pulse">Gallery</span>
+                    ) : (
+                        <label
+                            htmlFor="select"
+                            className="flex flex-row justify-between items-center gap-x-4"
+                        >
+                            <input
+                                type="checkbox"
+                                name="select"
+                                id="select"
+                                checked={selectPictures.length > 0}
+                                className="h-5 w-5 accent-blue-500 cursor-pointer"
+                                onChange={() => setSelectPictures([])}
+                            />
+                            {selectPictures.length} Files Selected
+                        </label>
+                    )}
+                </h1>
+                <button
+  className="text-red-500 font-medium hover:bg-red-600 hover:text-white hover:scale-105 transform transition-transform hover:duration-300 p-2 rounded-lg border border-red-600 hover:border-red-700"
+  onClick={handleDeleteFile}
+>
+  Delete files
+</button>
+
+
+            </article>
+        </nav>
+    );
 };
 
 export default GalleryHeader;
