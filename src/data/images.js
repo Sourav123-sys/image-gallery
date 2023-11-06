909fc419f1490140b8d5785b55d0f5207fcd4b85
Sourav123-sys <khansourav58@gmail.com>
2023-11-06 16:03:04 +0600

    data set

--- src/data/images.js
+++ src/data/images.js
@@ -1,17 +1,4 @@
-/**
- * Title: Write a program using JavaScript on Images
- * Author: Hasibul Islam
- * Portfolio: https://devhasibulislam.vercel.app
- * Linkedin: https://linkedin.com/in/devhasibulislam
- * GitHub: https://github.com/devhasibulislam
- * Facebook: https://facebook.com/devhasibulislam
- * Instagram: https://instagram.com/devhasibulislam
- * Twitter: https://twitter.com/devhasibulislam
- * Pinterest: https://pinterest.com/devhasibulislam
- * WhatsApp: https://wa.me/8801906315901
- * Telegram: devhasibulislam
- * Date: 01, November 2023
- */
+
 
 const images = [
     { id: 1, picture: "/assets/image-1.webp" },
