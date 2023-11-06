e1c901c2a270bfe1de0297cf5fd22b9b8bfd1059
Sourav123-sys <khansourav58@gmail.com>
2023-11-06 16:10:54 +0600

    data set

---
+++ src/data/images.js
@@ -0,0 +1,17 @@
+
+
+const images = [
+  { id: 1, picture: "/assets/image-1.webp" },
+  { id: 2, picture: "/assets/image-2.webp" },
+  { id: 3, picture: "/assets/image-3.webp" },
+  { id: 4, picture: "/assets/image-4.webp" },
+  { id: 5, picture: "/assets/image-5.webp" },
+  { id: 6, picture: "/assets/image-6.webp" },
+  { id: 7, picture: "/assets/image-7.webp" },
+  { id: 8, picture: "/assets/image-8.webp" },
+  { id: 9, picture: "/assets/image-9.webp" },
+  { id: 10, picture: "/assets/image-10.jpeg" },
+  { id: 11, picture: "/assets/image-11.jpeg" },
+];
+
+export default images;
